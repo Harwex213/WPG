520f6651c7a109d45ab44af0cf7d422c1cc625ac
unknown <you@example.com>
2020-06-07 23:29:12 +0300

    Update js.php

--- blocks/js.php
+++ blocks/js.php
@@ -1,4 +1,4 @@
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-<script src="<?php echo $path?>js/libs/headhesive.min.js"></script>
-<script src="<?php echo $path?>js/index.js"></script>
+<script src="../js/libs/headhesive.min.js"></script>
+<script src="../js/index.js"></script>
