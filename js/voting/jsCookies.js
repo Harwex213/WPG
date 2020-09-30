a588e79d3707ebc96586f151bcf1648c8b8071bd
Harwex213 <harwex213@mail.ru>
2020-09-30 18:23:31 +0300

    Update voting.php(almost make scss; none adaptive)

--- js/voting/jsCookies.js
+++ js/voting/jsCookies.js
@@ -1,55 +1,55 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $("a.vote_up").click(function() {
-        the_id = $(this).attr('id');
+	$("a.wpgAdv__vote-Up").click(function () {
+		the_id = $(this).attr('id');
 
-        // проверяем, есть ли запись о том, что vote + id уже нажимали
-        if ($.cookies.get("vote" + the_id))
-            $(".container_" + the_id).html("<span class='notVoted'>&#10008</span>");
+		// проверяем, есть ли запись о том, что vote + id уже нажимали
+		if ($.cookies.get("vote" + the_id))
+			$(".container_" + the_id).html("<span class='notVoted'>&#10008</span>");
 
-        else // Если нет, то тогда работаем дальше
-        {
-            $(".container_" + the_id).html("<span class='voted'>&#10004</span>");
-            $.ajax({
-                type: "POST",
-                data: "action=vote_up&id=" + $(this).attr("id"),
-                url: "C:\Denwer\home\custom\www\voting\voting.php",
-                success: function(msg) {
-                    // создаем файл cookies
-                    // $.cookies.set("vote" + the_id, 12345679, { hoursToLive: 168 });
-                }
-            });
-        }
-    });
+		else // Если нет, то тогда работаем дальше
+		{
+			$(".container_" + the_id).html("<span class='voted'>&#10004</span>");
+			$.ajax({
+				type: "POST",
+				data: "action=vote_up&id=" + $(this).attr("id"),
+				url: "../../voting/voting.php",
+				success: function (msg) {
+					// создаем файл cookies
+					// $.cookies.set("vote" + the_id, 12345679, { hoursToLive: 168 });
+				}
+			});
+		}
+	});
 
-    $("a.vote_down").click(function() {
-        the_id = $(this).attr('id');
+	$("a.wpgAdv__vote-Down").click(function () {
+		the_id = $(this).attr('id');
 
-        if ($.cookies.get("vote" + the_id)) $(".container_" + the_id).html("<span class='notVoted'>&#10008</span>");
-
-
-        else // Если нет, то тогда работаем дальше
-        {
-
-            $(".container_" + the_id).html("<span class='voted'>&#10004;</span>");
-
-            $.ajax({
-                type: "POST",
-                data: "action=vote_down&id=" + $(this).attr("id"),
-                url: "C:/Denwer/home/custom/www/voting/voting.php",
-                success: function(msg) {
-
-                    // $.cookies.set("vote" + the_id, 12345679, { hoursToLive: 168 });
-                }
-            });
-        }
-    });
+		if ($.cookies.get("vote" + the_id)) $(".container_" + the_id).html("<span class='notVoted'>&#10008</span>");
 
 
-    $(".imgs").click(function() {
-        the_id = $(this).attr('id');
-        console.log(the_id);
+		else // Если нет, то тогда работаем дальше
+		{
 
-        window.location.href = "../cards/card.php?id=" + the_id;
-    });
+			$(".container_" + the_id).html("<span class='voted'>&#10004;</span>");
+
+			$.ajax({
+				type: "POST",
+				data: "action=vote_down&id=" + $(this).attr("id"),
+				url: "../../voting/voting.php",
+				success: function (msg) {
+
+					// $.cookies.set("vote" + the_id, 12345679, { hoursToLive: 168 });
+				}
+			});
+		}
+	});
+
+
+	$(".imgs").click(function () {
+		the_id = $(this).attr('id');
+		console.log(the_id);
+
+		window.location.href = "../cards/card.php?id=" + the_id;
+	});
 });
--- voting/voting.php
+++ voting/voting.php
@@ -33,18 +33,18 @@ $path = '../';
 	</section>
 	<section class="list">
 		<div class="container">
-			<div class="title list__title">Рейтинговый список ВПИ</div>
+			<div class="title list__title"><p>Рейтинговый список ВПИ</p></div>
 			<div class="rank list__rank">
 				<div class="area rank__area">
 					<div class="area__info area__info-gold">Gold</div>
 					<div class="area__strip"></div>
-					<div class="area__strip area__strip"></div>
+					<div class="area__strip area__strip-left"></div>
 					<div class="area__strip area__strip-center"></div>
-					<div class="area__strip area__strip-left area__strip-center"></div>
+					<div class="area__strip area__strip-center-left area__strip-center"></div>
 					<div class="area__strip area__strip-down"></div>
 					<div class="area__strip area__strip-left area__strip-down"></div>
 				</div>
-				<div class="rank__fraction">1/3</div>
+				<div class="rank__fraction rank__fraction-left"><p>1/3</p></div>
 			</div>
 			<?php
 			$u = 1;//print transfer
@@ -61,13 +61,13 @@ $path = '../';
 						<div class="area rank__area">
 							<div class="area__info area__info-silver">Silver</div>
 							<div class="area__strip"></div>
-							<div class="area__strip area__strip"></div>
+							<div class="area__strip area__strip-left"></div>
 							<div class="area__strip area__strip-center"></div>
-							<div class="area__strip area__strip-left area__strip-center"></div>
+							<div class="area__strip area__strip-center-left area__strip-center"></div>
 							<div class="area__strip area__strip-down"></div>
 							<div class="area__strip area__strip-left area__strip-down"></div>
 						</div>
-						<div class="rank__fraction">1/5</div>
+						<div class="rank__fraction rank__fraction-left">1/5</div>
 					</div>
 					';
 				}
@@ -77,13 +77,13 @@ $path = '../';
 						<div class="area rank__area">
 							<div class="area__info area__info-bronz">Bronz</div>
 							<div class="area__strip"></div>
-							<div class="area__strip area__strip"></div>
+							<div class="area__strip area__strip-left"></div>
 							<div class="area__strip area__strip-center"></div>
-							<div class="area__strip area__strip-left area__strip-center"></div>
+							<div class="area__strip area__strip-center-left area__strip-center"></div>
 							<div class="area__strip area__strip-down"></div>
 							<div class="area__strip area__strip-left area__strip-down"></div>
 						</div>
-						<div class="rank__fraction">1/5</div>
+						<div class="rank__fraction rank__fraction-left">1/10</div>
 					</div>
 					';
 				}
@@ -93,13 +93,13 @@ $path = '../';
 						<div class="area rank__area">
 							<div class="area__info area__info-common">Common</div>
 							<div class="area__strip"></div>
-							<div class="area__strip area__strip"></div>
+							<div class="area__strip area__strip-left"></div>
 							<div class="area__strip area__strip-center"></div>
-							<div class="area__strip area__strip-left area__strip-center"></div>
+							<div class="area__strip area__strip-center-left area__strip-center"></div>
 							<div class="area__strip area__strip-down"></div>
 							<div class="area__strip area__strip-left area__strip-down"></div>
 						</div>
-						<div class="rank__fraction">1/5</div>
+						<div class="rank__fraction rank__fraction-left"></div>
 					</div>
 					';
 				}
@@ -110,64 +110,64 @@ $path = '../';
 						<div class="area rank__area">
 							<div class="area__info area__info-trash">Trash</div>
 							<div class="area__strip"></div>
-							<div class="area__strip area__strip"></div>
+							<div class="area__strip area__strip-left"></div>
 							<div class="area__strip area__strip-center"></div>
-							<div class="area__strip area__strip-left area__strip-center"></div>
+							<div class="area__strip area__strip-center-left area__strip-center"></div>
 							<div class="area__strip area__strip-down"></div>
 							<div class="area__strip area__strip-left area__strip-down"></div>
 						</div>
-						<div class="rank__fraction">1/5</div>
+						<div class="rank__fraction rank__fraction-left"></div>
 					</div>
 					';
 				}
 			?>
-			<div class="sample list__sample">
+			<div class="sample list__sample"> 
 				<span class="sample__position">#<?php echo $u ?></span>
 				<div class="wpgAdv sample__wpgAdv">
 					<img class="wpgAdv__avatar" id="<?php echo $row['id']; ?>" src="../img/logo_130/img<?php echo $row['id']; ?>.jpg">
 					<span class="wpgAdv__briefInfo">
-						<a class="wpgAdv__name" href='<?php echo $row['link']; ?>'> <?php echo $row['title']; ?> </a>
-						<p class="wpgAdv__type">РП/Калькулятор</p>
+						<p class="wpgAdv__name"><a href='<?php echo $row['link']; ?>'> <?php echo $row['title']; ?> </a></p>
+						<p class="wpgAdv__genre">Фэнтези: <span class="wpgAdv__genre-value">авторский мир</span></p>
+						<p class="wpgAdv__type">РП/Калькулятор<?php echo $row['type']; ?></p>
 					</span>
 					<?php
 					if($u <= 3 && $net_vote > 0) 
 					{
-						echo "<div class='gold container_"; 
+						echo "<div class='wpgAdv__votesCount wpgAdv__votesCount-gold container_"; 
 						echo $row['id'];
 						echo "'>";
 					}
 					elseif($u >= 4 && $u <= 8 && $net_vote > 0) 
 					{
-						echo "<div class='silver container_"; 
+						echo "<div class='wpgAdv__votesCount wpgAdv__votesCount-silver container_"; 
 						echo $row['id'];
 							echo "'>";
 					}
 					elseif($u >= 9 && $u < 19 && $net_vote > 0)
 					{
-						echo "<div class='bronze container_"; 
+						echo "<div class='wpgAdv__votesCount wpgAdv__votesCount-bronz container_"; 
 						echo $row['id'];
 						echo "'>";
 					}
 					elseif($u >= 19  && $net_vote >= 0)
 					{
-						echo "<div class='common container_"; 
+						echo "<div class='wpgAdv__votesCount wpgAdv__votesCount-common container_"; 
 						echo $row['id'];
 						echo "'>";
 					}
 					else
 					{
-						echo "<div class='trash container_"; 
+						echo "<div class='wpgAdv__votesCount-trash container_"; 
 						echo $row['id'];
 						echo "'>";
 					}
 					?>
-					<span class='votes_count' id='votes_count_<?php echo $row['id']; ?>'><?php echo $net_vote; ?></span>
+						<span id='votes_count_<?php echo $row['id']; ?>'><?php echo $net_vote; ?></span>
 					</div>
-					<div class='vote_buttons' id='vote_buttons<?php echo $row['id']; ?>'>
-						<a href='javascript:;' class='vote_up' id='<?php echo $row['id']; ?>'><p class='markVote'>+1</p></a>
-						<a href='javascript:;' class='vote_down' id='<?php echo $row['id']; ?>'><p class='markVote'>-1</p></a>
+					<div class='wpgAdv__voteButtons' id='vote_buttons<?php echo $row['id']; ?>'>
+						<a href='javascript:;' class='wpgAdv__vote wpgAdv__vote-Up' id='<?php echo $row['id']; ?>'>+1</a>
+						<a href='javascript:;' class='wpgAdv__vote wpgAdv__vote-Down' id='<?php echo $row['id']; ?>'>-1</a>
 					</div>
-					<div  class='RP'><?php echo $row['type']; ?></div>
 				</div>
 			</div>
 			<?php
@@ -175,9 +175,9 @@ $path = '../';
 			}
 			else echo 'Ошибка чтения данных с БД';
 			?>
+		<!-- Что это за div-ы? -->
 		</div>
 		</div>
-	</div>
 	</section>
 	<!-- END WRAPPER -->
 </div>
